916ad39d65b06b570dc41539208c6ba6f8dbe0ea
Aliaksandr <lipskialiakasandr@gmail.com>
2021-11-07 00:49:16 +0300

    sass

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -1,22 +1,30 @@
 import React from "react";
 import styleContainer from '../common/styles/Container.module.css';
-import style from './Projects.module.css';
-import Project from "./project/Project";
-
+import style from './Projects.module.scss';
+import Project from './project/Project';
+import Title from '../common/components/title/Title';
+import todoImage from './../assets/image/Todolist.jpg'
 
 
 const Projects = () => {
- return (
-     <div className={style.projectsBlock}>
-         <div className={`${styleContainer.container} ${style.projectsContainer}`}>
-             <h2 className={style.title}>Projects</h2>
-             <div className={style.projects}>
-                 <Project title={"Social network"} description={"sed do eiusmod tempor..."}/>
-                 <Project title={"Todo list"} description={"Lorem ipsum dolor sit amet, consectetur..."}/>
-             </div>
-         </div>
-     </div>
- )
+    const social = {
+        backgroundImage: `url(${todoImage})`,
+    }
+    const todolist = {
+        backgroundImage: 'url(' + todoImage + ')',
+    }
+    return (
+        <div className={style.projectsBlock}>
+            <div className={`${styleContainer.container} ${style.projectsContainer}`}>
+                <Title text={"Projects"}/>
+                <div className={style.projects}>
+                    <Project style={social} title={"Social network"} description={"sed do eiusmod tempor..."}/>
+                    <Project style={todolist} title={"Todo list"}
+                             description={"Lorem ipsum dolor sit amet, consectetur..."}/>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Projects;
--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -2,17 +2,19 @@ import React from "react";
 import style from './Skills.module.css';
 import styleContainer from "../common/styles/Container.module.css";
 import Skill from "./skill/Skill";
+import Title from "../common/components/title/Title";
 
 
 const Skills = () => {
     return (
         <div className={style.skillsBlock}>
             <div className={`${styleContainer.container} ${style.skillsContainer}`}>
-                <h2 className={style.title}>Skills</h2>
+                <Title text={"Skills"}/>
                 <div className={style.skills}>
                     <Skill title={"Js"} description={"...there should be text here..."}/>
                     <Skill title={"Redux"} description={"...there should be text here...there should be text here..."}/>
-                    <Skill title={"React"} description={"...there should be text here...there should be text here...there should be text here..."}/>
+                    <Skill title={"React"}
+                           description={"...there should be text here...there should be text here...there should be text here..."}/>
                 </div>
             </div>
         </div>
